7431390511d54fabdbd55dc73a078f667f1694d2
tlee08 <timothyeelim.lee@gmail.com>
2026-03-24 02:00:59 +1100

    abcd

--- src/cellcounter/utils/dask_utils.py
+++ src/cellcounter/utils/dask_utils.py
@@ -160,29 +160,6 @@ def da_trim(arr, d=DEPTH):
     )
 
 
-def cluster_proc_dec(cluster_factory: Callable[[], SpecCluster]):
-    """`cluster_factory` is a function that returns a Dask cluster.
-
-    This function makes the Dask cluster and client, runs the function,
-    then closes the client and cluster.
-    """
-
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            cluster = cluster_factory()
-            client = Client(cluster)
-            print(client.dashboard_link)
-            res = func(*args, **kwargs)
-            client.close()
-            cluster.close()
-            return res
-
-        return wrapper
-
-    return decorator
-
-
 @contextlib.contextmanager
 def cluster_process(cluster: SpecCluster):
     """Context manager that runs the given Dask cluster and a client.
