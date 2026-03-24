64428146bdbd2143ff820453dedf043202fe943b
tlee08 <timothyeelim.lee@gmail.com>
2026-03-25 01:12:00 +1100

    abcd

--- src/cellcounter/constants.py
+++ src/cellcounter/constants.py
@@ -105,7 +105,7 @@ ATLAS_DIR = CACHE_DIR / "atlas_resources"
 # Checking whether dask_cuda works (i.e. is Linux and has CUDA)
 DASK_CUDA_ENABLED = importlib.util.find_spec("dask_cuda") is not None
 # Checking whether gpu extra dependency (CuPy) is installed
-GPU_ENABLED = importlib.util.find_spec("cupy") is not None
+CUPY_ENABLED = importlib.util.find_spec("cupy") is not None
 
 # Setting Dask configuration
 dask.config.set(
