8e9fec3b2117a19bf9a3f889858c10572dc06fee
tlee08 <timothyeelim.lee@gmail.com>
2026-03-26 01:59:24 +1100

    abcd

--- src/cellcounter/pipeline/pipeline.py
+++ src/cellcounter/pipeline/pipeline.py
@@ -122,7 +122,7 @@ class Pipeline(AbstractPipeline):
             dask.delayed(self.cellc_funcs.get_boundary_pairs)(i)
             for i in label_overlap.to_delayed().ravel()
         ]
-        pair_arr_ls = [dask.compute(i) for i in delayed_ls]
+        pair_arr_ls = [dask.compute(i)[0] for i in delayed_ls]
         pairs_arr = (
             np.concatenate(pair_arr_ls, axis=0) if pair_arr_ls else np.empty((0, 2))
         )
@@ -137,7 +137,7 @@ class Pipeline(AbstractPipeline):
             dask.delayed(self.cellc_funcs.get_label_sizemap)(i)
             for i in label_arr.to_delayed().ravel()
         ]
-        label_counts_ls = [dask.compute(i) for i in delayed_ls]
+        label_counts_ls = [dask.compute(i)[0] for i in delayed_ls]
         label_counts = (
             np.concatenate(label_counts_ls, axis=0)
             if label_counts_ls
