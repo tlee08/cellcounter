737f72b2e161364b4b98409f7652e60e683eefd6
tlee08 <timothyeelim.lee@gmail.com>
2026-03-31 20:34:51 +1100

    abcd

--- src/cellcounter/models/proj_config/visual_check_config.py
+++ src/cellcounter/models/proj_config/visual_check_config.py
@@ -9,7 +9,7 @@ class VisualCheckConfig(BaseModel):
     heatmap_raw_radius: PositiveInt = 5
     heatmap_trfm_radius: PositiveInt = 3
     cellcount_trim: DimsSliceConfig = DimsSliceConfig(
-        z=SliceConfig(start=0, stop=10, step=None),
+        z=SliceConfig(start=500, stop=510, step=None),
         y=SliceConfig(),
         x=SliceConfig(),
     )
